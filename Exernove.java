ab1a01f0b9b00bfce9c39bedb25d785436b2d49a
Carlosaugustob <carlosaugustobborgess@gmail.com>
2018-04-20 11:21:17 -0300

    alguns ajustes e inicio da lista

--- Exernove.java
+++ Exernove.java
@@ -4,20 +4,36 @@ import javax.swing.JOptionPane;
     public static void main(String[] args){
 
     
- int opcao = Integer.parseIntJOptionPane.showMessageDialog
-    (null, "Tipo"));
- int opcao2 = Integer.parseIntJOptionPane.showMessageDialog(null, "Nome"));
- int opcao3 = Integer.parseIntJOptionPane.showMessageDialog(null, "Valor"));
- 
-    "Bolo      | Bolo Brigadeiro        | R$ 29,50" + 
-"\n""Bolo      |Bolo Floresta Negra        R$ 2,00" +
-"\n""Bolo      |Bolo Leite com Nutella        R$ 29,23" + 
-"\n""Bolo      |Bolo Mousse de Chocolate        R$ 7,10" +
-"\n""Bolo      |Bolo Nega Maluca        R$ 19,33" +
-"\n""Bolo      |Bomba de Creme          R$ 17,71" +
-"\n""Bolo      |Bomba de Morango        R$  4,82" + 
-"\n""Sanduíches      |Filé-Mignon com fritas e cheedar        R$ 21,16"  +
-"\n""Sanduíches      |Hambúrguer com queijos,champignon e rúcula        R$ 12,70" +
-"\n""Sanduíches      |Provolone com salame        R$ 19,70"  +
-"\n""Sanduíches      |Vegetariano de berinjela        R$ 28,22" +
-"\n""Pizzas      |Calabresa        R$      
+
+ int opcao = Integer.parseInt(JOptionPane.showInputDialog(null, 
+ "Tipo        |                                       Nome                                  Preço " + 
+ "\nBolo      |                                1- Bolo Brigadeiro                       R$ 29,50" + 
+"\nBolo       |                         2- Bolo Floresta Negra                      R$ 2,00" +
+"\nBolo       |                    3- Bolo Leite com Nutella                      R$ 29,23" + 
+"\nBolo       |                    4- Bolo Mousse de Chocolate                      R$ 7,10" +
+"\nBolo       |                                 5- Bolo Nega Maluca                      R$ 19,33" +
+"\nBolo       |                                   6- Bomba de Creme                      R$ 17,71" +
+"\nBolo       |                                 7- Bomba de Morango                      R$  4,82" + 
+"\nSanduíches |                 8- Filé-Mignon com fritas e cheedar                      R$ 21,16"  +
+"\nSanduíches |       9- Hambúrguer com queijos,champignon e rúcula                R$ 12,70" +
+"\nSanduíches |                            10- Provolone com salame                      R$ 19,70"  +
+"\nSanduíches |                        11- Vegetariano de berinjela                      R$ 28,22" +
+"\nPizzas     |                                     12- Calabresa                        R$ 8,98" + 
+"\nPizzas     |                                     13- Napolitana                       R$ 0,42"    +
+"\nPizzas     |                                     14- Peruana                          R$ 18,36" +
+"\nPizzas     |                                     15- Portuguesa                       R$ 27,50")); 
+    
+    
+    
+double opcao = double.parseDouble
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+ }
+
