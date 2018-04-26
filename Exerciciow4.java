9d3c33c355b11163e6764a36c1f53684158a3cb9
Carlosaugustob <38558653+Carlosaugustob@users.noreply.github.com>
2018-04-26 11:25:27 -0300

    descrição
    começo do exer 4

---
+++ Exerciciow4.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+  public class Exerciciow4{
+
+        public static void main(String[] args){
+
+    int quantidade = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de carros que deseja cadastrar: "));
+    int carros = 0;
+    carros
+        }
+        }
+
+
+        
